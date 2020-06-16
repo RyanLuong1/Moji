4ba8f5bd243fd847cbdf605a8af498e8b3317201
Ryan Luong <ryanluong83@gmail.com>
2020-06-16 11:03:07 -0700

    Added the ability to display a person's server emotes

--- emojibot.py
+++ emojibot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
@@ -11,9 +12,12 @@ emojiBot = commands.Bot(command_prefix = '!')
 @emojiBot.command(name='emojis')
 async def emojis(ctx):
     emojisList = []
+    # emojisString = ""
     for emoji in ctx.guild.emojis:
-        emojisList.append(str(emoji.id))
-    a = (" ".join(emojisList))
-    await ctx.send(a)
+        emojisList.append(emojiBot.get_emoji(emoji.id))
+    emojis = "";
+    for emoji in emojisList:
+        emojis += f'{emoji}'
+    await ctx.send(emojis)
 
 emojiBot.run(TOKEN)
